234c24586fd163222fe447f51d6583fcc8391adf
Yuchen <davidzhang1107@163.com>
2019-12-10 00:06:10 +0800

    add solr requests and canned/index page

--- canned/views.py
+++ canned/views.py
@@ -1,10 +1,19 @@
+import json
+import requests 
+
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+
+URL = 'http://localhost:8983/solr/gettingstarted/select?q={}'
+query = 'book'
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hi bro!')
-    # return render(request, 'canned/index.html', {'answer': get_solr_response(1)} )
+
+    # return HttpResponse('Hi bro!')
+    return render(request, 'canned/index.html', {'answer': get_solr_response(query)} )
 
 def get_solr_response(query):
-    return 'Hi bro!'
+    res = requests.get(URL.format(query), verify=False)
+    txt = json.loads(res.text)
+    return txt
